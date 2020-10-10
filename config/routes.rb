222dcd5764fb292bf7d0939d88211f1e3bf60d5a
yoichi3223 <yoichi3223@gmail.com>
2020-10-10 07:36:20 +0000

    list add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources:user, only: %i(edit update)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
+  resources:list, only: %i(new create edit update destroy)
 end
